a74c2147fc9c2e880247dd0549993ebc5345f7de
jankes44 <kalima12345@gmail.com>
2026-01-05 17:25:29 +0100

    edit reload-quest-restart.sh

--- reload-quest-restart.sh
+++ reload-quest-restart.sh
@@ -1,8 +1,8 @@
-cd locale/english/quest/
-cd ../../../
 python3 stop.py
 python3 clear.py
+cd locale/english/quest/
 python3 make.py
+cd ../../../
 python3 perms.py
 python3 start.py
 echo "Quest reload and server restart complete."
