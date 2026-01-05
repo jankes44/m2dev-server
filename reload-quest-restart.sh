b38e7ba77210d840d393d2f0b0d904d31599fa6a
jankes44 <kalima12345@gmail.com>
2026-01-05 16:49:15 +0100

    add bravery cloak, removry cloaks, remove debug lines from teleport.quest

--- reload-quest-restart.sh
+++ reload-quest-restart.sh
@@ -1,7 +1,8 @@
 cd locale/english/quest/
-python3 make.py
 cd ../../../
 python3 stop.py
 python3 clear.py
+python3 make.py
+python3 perms.py
 python3 start.py
 echo "Quest reload and server restart complete."
